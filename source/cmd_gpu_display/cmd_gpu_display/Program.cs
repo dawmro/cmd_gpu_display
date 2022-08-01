fd4bc34f5e6d2ee526129dda769c9c4252aa854d
dawid mrotek <DawidMrotek@protonmail.ch>
2022-08-01 23:31:21 +0200

    Added initial source code

---
+++ source/cmd_gpu_display/cmd_gpu_display/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cmd_gpu_display
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Clean previous data");
+                Console.WriteLine("Get data for all gpus");
+                Console.WriteLine("Print data");
+                Console.WriteLine("Wait few seconds");
+                System.Threading.Thread.Sleep(5000);
+
+            }
+        }
+    }
+}
